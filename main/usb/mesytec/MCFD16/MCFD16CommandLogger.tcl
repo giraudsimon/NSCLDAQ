52c617595d5597e61a983a57036ecb83d0b7b9d6
Jeromy Tompkins <tompkins@nscl.msu.edu>
2017-03-21 14:25:59 -0400

    Adds global coinc time support to cmd logger / memorizer

--- main/usb/mesytec/MCFD16/MCFD16CommandLogger.tcl
+++ main/usb/mesytec/MCFD16/MCFD16CommandLogger.tcl
@@ -218,6 +218,13 @@ snit::type MCFD16CommandLogger {
     chan puts $_logFile "[$self cget -name] GetFastVeto"
   }
 
+  method SetGlobalCoincTime {value} {
+    chan puts $_logFile "[$self cget -name] SetGlobalCoincTime $value"
+  }
+
+  method GetGlobalCoincTime {} {
+    chan puts $_logFile "[$self cget -name] GetGlobalCoincTime"
+  }
 
   ## @brief Convenience wrapper around a "chan flush" command
   method Flush {} {
