d5a0c144c1ed1da2c7a15bd02d99d2a596eee582
Jeromy Tompkins <tompkins@nscl.msu.edu>
2017-05-03 10:13:24 -0400

    Improves the tcl Process package

--- main/base/tclutils/Process.tcl
+++ main/base/tclutils/Process.tcl
@@ -20,23 +20,34 @@
 # ############################################################
 
 
-package provide Process 1.0
+package provide Process 2.0
 
 package require snit
+
+puts "Temporary: Process 2.0"
 
 ## @brief Encapsulation of a pipeline process
 #
 # This provides the user with a simple way to handle 
-# pipelines. The -oneof option is a callback that allows 
-# user-defined code to execute when the channel is closed.
+# pipelines. The pipeline is opened during construction of the Process
+# instance. The process that actually is run as a pipeline is defined
+# by the user with the -command option. The stdout and stderr outputs of the process 
+# specified by that option are piped to the cat program, which in turn writes the output
+# to stdout. There are two different callbacks that the user
+# can register for different events:
 #
-# It is not possible to open more than one channel per 
-# instance of this snit::type.
+# -oneof   takes a script to execute when pipeline reaches EOF condition
+# -onread  take a script to execute when output has been read from the file.
+#          The script should accept a single argument that is the string
+#          of output.
+#
+# It is not possible to open more than one pipeline per instance of this snit::type.
 #
 snit::type Process {
   option -command {}
   option -oneof {}
-  
+  option -onread {}
+
   variable fd {}
 
   ## @brief Open the pipeline and register the readable callback
@@ -52,16 +63,25 @@ snit::type Process {
   ##  Close the open channel if it is still open
   destructor {
     if {$fd ne {}} {
+      set pids [pid $fd]
+      foreach p $pids {
+        exec kill $p
+      }
       catch {close $fd}
+      set fd {}
     }
   }
 
   ## @brief Callback for readable events on the open pipe
   #
-  # If the channel is not at an EOF condition, this simply
-  # prints the output to stdout. Otherwise, the script specified
-  # by the -oneof (i.e. on end-of-file) option is executed in
-  # the global scope.
+  # On end of file events, the pipeline is closed. After the channel is closed, 
+  # the -oneof script is called in the global scope, if it is defined. 
+  #
+  # If the channel is not in an EOF condition, the script defined by -onread
+  # is called in the global scope with the line read from the pipeline as its argument.
+  #
+  # \param channel  the file channel to read from
+  #
   method onReadable channel {
     chan gets $channel line 
     if {[eof $channel]} {
@@ -69,7 +89,9 @@ snit::type Process {
       set fd {}
       uplevel #0 $options(-oneof) 
     } else {
-      puts $line
+      if {$options(-onread) ne {}} {
+        uplevel #0 $options(-onread) $line
+      }
     }
   }
 
