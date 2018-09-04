b6657156f0f1712eac9b2ade4d29962b60b0d421
Ron Fox <fox@nscl.msu.edu>
2018-09-04 17:18:52 -0400

    Add new tests for fragment allocation.  Disable the evbrdocallouts10 tests...no longer supporting that.

--- main/daq/eventbuilder/tclTests.tcl
+++ main/daq/eventbuilder/tclTests.tcl
@@ -3,7 +3,8 @@
 exec tclsh "$0" "$@"
 
 package require tcltest
-::tcltest::configure -testdir [file dirname [file normalize [info script]]]
+::tcltest::configure -testdir [file dirname [file normalize [info script]]] \
+		     -skip rdo10*
 tcltest::testConstraint false 0
 eval ::tcltest::configure $argv
 
