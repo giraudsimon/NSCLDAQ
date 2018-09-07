5acc904628f1591785ef0a62f84f574bb803c010
Ron Fox <fox@nscl.msu.edu>
2018-09-07 12:48:24 -0400

    This should be in the repo

---
+++ main/daq/eventbuilder/CopyPopUntil.h
@@ -0,0 +1,55 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Jeromy Tompkins 
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  CopyPopUntil.h
+ *  @brief: Templated function to copy/pop from the front of a container until
+ *          a predicate is true.
+ */
+
+#ifndef COPYPOPUNTIL_H
+#define COPYPOPUNTIL_H
+#include <algorithm>
+
+
+
+/**
+ * CopyPopUntil
+ *    pushes back the front of a container to the back of another container
+ *    popping until a predicate is true.
+ *
+ * @param c1 - container 1 must support front(), empty() and pop_front().
+ * @param c2 - container 2 must support push_back and contain the same type
+ *             of item as c1.
+ * @param pred - Predicate called with a reference to the item contained
+ *             returns true when done copying.
+ */
+template <class c1type, class c2type, class UnaryPredicate>
+void
+CopyPopUntil(c1type& c1, c2type& c2, UnaryPredicate& pred)
+{
+    while (!c1.empty()) {
+        if (! pred(c1.front())) {
+            c2.push_back(c1.front());
+            c1.pop_front();
+        } else {
+            return;
+        }
+    }
+}
+
+
+#endif
