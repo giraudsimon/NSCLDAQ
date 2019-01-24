62f5b9cdd89824bc66bc0ef9e767ee50eb96efb5
Ron Fox <fox@nscl.msu.edu>
2019-01-24 10:28:04 -0500

    Test setup built - fake data source is in the class may want factoring later

--- main/utilities/swtrigger/swfiltersrctests.cpp
+++ main/utilities/swtrigger/swfiltersrctests.cpp
@@ -2,27 +2,62 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include "swFilterRingDataSource.h"
+#include <CDataSource.h>
+#include <CRingItem.h>
+
+#include <list>
+
+// mock for CDataSource
+
+class CFakeDataSource : public CDataSource
+{
+private:
+  std::list<CRingItem*> m_items;
+public:
+  virtual ~CFakeDataSource() {}
+  void addItem(CRingItem* pItem);    // Must have been new'd.
+  CRingItem* getItem() {
+    CRingItem* result(nullptr);    // result if there's nothing left.
+    
+    if(!m_items.empty()) {         // but if there is...
+      result = m_items.front();
+      m_items.pop_front();
+    }
+    
+    return result;
+  }
+  void read(char* pBuffer, size_t nBytes) {}
+};
 
 
-
-class Testname : public CppUnit::TestFixture {
-  CPPUNIT_TEST_SUITE(Testname);
+class ringsourceTests : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(ringsourceTests);
   CPPUNIT_TEST(aTest);
   CPPUNIT_TEST_SUITE_END();
 
 
 private:
-
+  CFakeDataSource*        m_pSrc;
+  swFilterRingDataSource* m_pTestObject;
 public:
   void setUp() {
+    m_pSrc = new CFakeDataSource;
+    m_pTestObject = new swFilterRingDataSource(*m_pSrc);
   }
   void tearDown() {
+    delete m_pTestObject;
+    delete m_pSrc;
+    
+    m_pTestObject = nullptr;
+    m_pSrc        = nullptr;
   }
 protected:
   void aTest();
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(Testname);
+CPPUNIT_TEST_SUITE_REGISTRATION(ringsourceTests);
 
-void Testname::aTest() {
+void ringsourceTests::aTest() {
 }
