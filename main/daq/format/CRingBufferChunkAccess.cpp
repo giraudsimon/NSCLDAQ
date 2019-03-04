416884cafc201c5343820b4e0e4044512b9c11ca
Ron Fox <fox@nscl.msu.edu>
2019-03-04 11:17:14 -0500

    Progress testing CRingBufferChunkAccess - iterator testing.

--- main/daq/format/CRingBufferChunkAccess.cpp
+++ main/daq/format/CRingBufferChunkAccess.cpp
@@ -402,11 +402,20 @@ ChunkIterator::operator*()
 ChunkIterator&
 ChunkIterator::operator++()
 {
-    pRingItemHeader pH = (operator->());
-    uint8_t*        p  = static_cast<uint8_t*>(m_pData);
-    p                 += pH->s_size;
-    m_pData = p;
-    
+    if(m_pData) {                         // Don't increment end.
+        pRingItemHeader pH = (operator->());
+        m_nOffset += pH->s_size;
+        if(m_nOffset < m_nTotalBytes) {    
+        
+            uint8_t*        p  = static_cast<uint8_t*>(m_pData);
+            p                 += pH->s_size;
+            m_pData = p;
+        } else {                      // End.
+            m_pData = nullptr;
+            m_nOffset = 0;
+            m_nTotalBytes = 0;
+        }
+    }    
     return *this;
 }
 /**
--- main/daq/format/rbchunktests.cpp
+++ main/daq/format/rbchunktests.cpp
@@ -17,6 +17,15 @@ class rbchunkTest : public CppUnit::TestFixture {
   // Iterator tests:
   CPPUNIT_TEST(iconst_1);
   CPPUNIT_TEST(iconst_2);
+  CPPUNIT_TEST(iconst_3);
+  CPPUNIT_TEST(assignment);
+  CPPUNIT_TEST(equals_1);
+  CPPUNIT_TEST(equals_2);
+  CPPUNIT_TEST(deref_1);
+  CPPUNIT_TEST(deref_2);
+  CPPUNIT_TEST(preinc_1);
+  CPPUNIT_TEST(preinc_2);
+  CPPUNIT_TEST(preinc_3);
   CPPUNIT_TEST_SUITE_END();
 
 
@@ -30,6 +39,15 @@ public:
 protected:
   void iconst_1();
   void iconst_2();
+  void iconst_3();
+  void assignment();
+  void equals_1();
+  void equals_2();
+  void deref_1();
+  void deref_2();
+  void preinc_1();
+  void preinc_2();
+  void preinc_3();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(rbchunkTest);
@@ -48,4 +66,106 @@ void rbchunkTest::iconst_2()
   EQ((void*)(buffer), i.m_pData);
   EQ(size_t(0), i.m_nOffset);
   EQ(size_t(100), i.m_nTotalBytes);
+}
+// Copy construction
+
+void rbchunkTest::iconst_3()
+{
+  uint8_t buffer[100];
+  CRingBufferChunkAccess::Chunk::iterator j(buffer, 100);
+  CRingBufferChunkAccess::Chunk::iterator i(j);
+  EQ((void*)(buffer), i.m_pData);
+  EQ(size_t(0), i.m_nOffset);
+  EQ(size_t(100), i.m_nTotalBytes);
+  
+}
+
+// Assignment:
+
+void rbchunkTest::assignment()
+{
+  uint8_t buffer[100];
+  CRingBufferChunkAccess::Chunk::iterator j(buffer, 100);
+  CRingBufferChunkAccess::Chunk::iterator i(nullptr, 0);
+   
+  i = j;
+  
+  EQ((void*)(buffer), i.m_pData);
+  EQ(size_t(0), i.m_nOffset);
+  EQ(size_t(100), i.m_nTotalBytes);
+}
+// They are equal:
+
+void rbchunkTest::equals_1()
+{
+  uint8_t buffer[100];
+  CRingBufferChunkAccess::Chunk::iterator j(buffer, 100);
+  CRingBufferChunkAccess::Chunk::iterator i(j);
+ 
+  ASSERT(j == i);
+}
+// Not equal
+
+void rbchunkTest::equals_2()
+{
+  uint8_t buffer[100];
+  CRingBufferChunkAccess::Chunk::iterator j(buffer, 100);
+  CRingBufferChunkAccess::Chunk::iterator i(nullptr, 0);
+  ASSERT(!(i == j));
+}
+
+void rbchunkTest::deref_1()
+{
+  RingItemHeader h = {2*sizeof(uint32_t), PHYSICS_EVENT};
+  CRingBufferChunkAccess::Chunk::iterator p(&h, sizeof(h));
+  
+  EQ(uint32_t(2*sizeof(uint32_t)), p->s_size);
+  EQ(PHYSICS_EVENT, p->s_type);
+}
+
+void rbchunkTest::deref_2()
+{
+  RingItemHeader h = {2*sizeof(uint32_t), PHYSICS_EVENT};
+  CRingBufferChunkAccess::Chunk::iterator p(&h, sizeof(h));
+
+  RingItemHeader& href(*p);
+  EQ(uint32_t(2*sizeof(uint32_t)), href.s_size);
+  EQ(PHYSICS_EVENT, href.s_type);
+}
+void rbchunkTest::preinc_1()
+{
+  // no change for end:
+  
+  CRingBufferChunkAccess::Chunk::iterator p(nullptr, 0);  // End.
+  CRingBufferChunkAccess::Chunk::iterator& q(++p);
+  
+  ASSERT(&q == &p);             // Same objects since ++p gives ref.
+  EQ((void*)(nullptr), p.m_pData);
+  EQ(size_t(0), p.m_nOffset);
+  EQ(size_t(0), p.m_nTotalBytes); 
+}
+void rbchunkTest::preinc_2()
+{
+  uint32_t buffer[100];
+  buffer[0] = 4*sizeof(uint32_t); // Simple ring item.
+  buffer[1] = PHYSICS_EVENT;
+  buffer[4] = 0x1234;
+  
+  CRingBufferChunkAccess::Chunk::iterator p(buffer, 100*sizeof(uint32_t));
+  ++p;
+  EQ(buffer[4], p->s_size);
+  
+}
+void rbchunkTest::preinc_3()            // increment into end.
+{
+  uint32_t buffer[10];
+  buffer[0] = 10*sizeof(uint32_t);
+  buffer[1] = PHYSICS_EVENT;
+  CRingBufferChunkAccess::Chunk::iterator p(buffer, 10*sizeof(uint32_t));
+  ++p;
+  // End of iteration.
+  
+  EQ((void*)(nullptr), p.m_pData);
+  EQ(size_t(0), p.m_nOffset);
+  EQ(size_t(0), p.m_nTotalBytes);   
 }
