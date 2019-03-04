b67fdb8efa0825619a988b6894820054bc61477a
Ron Fox <fox@nscl.msu.edu>
2019-03-04 10:42:29 -0500

    Iterator construction tests -- mode changes

--- main/daq/format/rbchunktests.cpp
+++ main/daq/format/rbchunktests.cpp
@@ -2,12 +2,21 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
+#include "Asserts.h"
+
+#define private public
+#include "CRingBufferChunkAccess.h"
+#undef private
+
+#include <stdint.h>
 
 
-
-class Testname : public CppUnit::TestFixture {
-  CPPUNIT_TEST_SUITE(Testname);
-  CPPUNIT_TEST(aTest);
+class rbchunkTest : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(rbchunkTest);
+  
+  // Iterator tests:
+  CPPUNIT_TEST(iconst_1);
+  CPPUNIT_TEST(iconst_2);
   CPPUNIT_TEST_SUITE_END();
 
 
@@ -19,10 +28,24 @@ public:
   void tearDown() {
   }
 protected:
-  void aTest();
+  void iconst_1();
+  void iconst_2();
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(Testname);
+CPPUNIT_TEST_SUITE_REGISTRATION(rbchunkTest);
 
-void Testname::aTest() {
+void rbchunkTest::iconst_1() {
+  CRingBufferChunkAccess::Chunk::iterator i(nullptr, 0);  // actually end.
+  EQ((void*)(nullptr), i.m_pData);
+  EQ(size_t(0), i.m_nOffset);
+  EQ(size_t(0), i.m_nTotalBytes);
+}
+
+void rbchunkTest::iconst_2()
+{
+  uint8_t buffer[100];
+  CRingBufferChunkAccess::Chunk::iterator i(buffer, 100);
+  EQ((void*)(buffer), i.m_pData);
+  EQ(size_t(0), i.m_nOffset);
+  EQ(size_t(100), i.m_nTotalBytes);
 }
