979e57978c4a7dc4dc160000611046084cf30822
Ron Fox <fox@nscl.msu.edu>
2019-03-06 14:01:40 -0500

    *  Good compilation on vector writeData method. *  Moved ringsource -> ringsource.old to prepare for rewrite. *  Got the ggo part of the ringsource to compile properly with the new    directory and new Makefile.am.

--- main/base/os/io.h
+++ main/base/os/io.h
@@ -13,22 +13,13 @@
 	     Michigan State University
 	     East Lansing, MI 48824-1321
 */
-#ifndef __IO_H
-#define __IO_H
+#ifndef IO_H
+#define IO_H
 
-#ifndef __CRT_UNISTD_H
 #include <unistd.h>
-#ifndef __CRT_UNISTD_H
-#define __CRT_UNISTD_H
-#endif
-#endif
-
-#ifndef __CRT_STDINT_H
 #include <stdint.h>
-#ifndef __CRT_STDINT_H
-#define __CRT_STDINT_H
-#endif
-#endif
+
+#include<sys/uio.h>
 
 #include <set>
 /**
@@ -41,7 +32,7 @@ namespace io {
   void writeData (int fd, const void* pData , size_t size);
   size_t readData (int fd, void* pBuffer,  size_t nBytes);
   void closeUnusedFiles(std::set<int> keepOpen);
-  
+  void writeData(int fd, struct iovec* iov, int iovcnt);
 }
 
 
