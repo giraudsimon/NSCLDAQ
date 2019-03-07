2b923a16c5274d65a47675eefcc935e5a4cc35ab
Ron Fox <fox@nscl.msu.edu>
2019-03-06 23:39:51 -0500

    Finish testing what we can of tghe vector io::writeData

--- main/base/os/testiov.cpp
+++ main/base/os/testiov.cpp
@@ -18,6 +18,11 @@ updateIov(struct iovec* iov, int& nItems, ssize_t nBytes);
 class iovtest : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(iovtest);
   CPPUNIT_TEST(full_1);
+  CPPUNIT_TEST(full_2);
+  
+  CPPUNIT_TEST(partial_1);
+  CPPUNIT_TEST(partial_2);
+  CPPUNIT_TEST(partial_3);
   CPPUNIT_TEST_SUITE_END();
 
 
@@ -30,6 +35,11 @@ public:
   }
 protected:
   void full_1();
+  void full_2();
+  
+  void partial_1();
+  void partial_2();
+  void partial_3();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(iovtest);
@@ -42,4 +52,50 @@ void iovtest::full_1() {
   io::updateIov(&iov, n, 100);
   
   EQ(0, n);
+}
+
+void iovtest::full_2() {
+  struct iovec iov[2] {
+    {(void*)(nullptr), 50},
+    {(void*)(nullptr), 50}
+  };
+  int n = 2;
+  io::updateIov(iov, n, 100);
+  EQ(0, n);
+}
+
+void iovtest::partial_1() { // one vector element partially done.
+  struct iovec iov {
+    0, 100
+  };
+  int n = 1;
+  io::updateIov(&iov, n, 50);
+  EQ(1, n);
+  EQ(size_t(50), iov.iov_len);
+  EQ((void*)(50), iov.iov_base);
+  
+}
+void iovtest::partial_2() {  // Two vector elements, the first is fully done:
+  struct iovec iov[2]{
+    {0, 100},
+    {0, 200}
+  };
+  int n = 2;
+  struct iovec* next = io::updateIov(iov, n, 100);
+  EQ(1, n);
+  EQ(&(iov[1]), next);
+  EQ(size_t(200), next->iov_len);
+  
+}
+void iovtest::partial_3() { // Two vector elements, first and part of second done.
+  struct iovec iov[2]{
+    {0, 100},
+    {0, 200}
+  };
+  int n = 2;
+  struct iovec* next = io::updateIov(iov, n, 200);  
+  
+  EQ(1, n);
+  EQ(&(iov[1]), next);
+  EQ(size_t(100), next->iov_len);
 }
