8dbff64884efec6c771768534a2ea2f97ac5f453
Ron Fox <fox@nscl.msu.edu>
2019-03-20 12:34:26 -0400

    Add/debugged event builder file checker for case where dt is 1, two sources 0,2 timestamps increment by one each event.

--- perftools/evbfilecheck.cpp
+++ perftools/evbfilecheck.cpp
@@ -42,15 +42,18 @@
 #include <Exception.h>                // Base class for exception handling.
 #include <CRingItemFactory.h>
 
+#include <FragmentIndex.h>        // From SpecTcl 5.x  e.g
 
 // standard run time headers:
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <cstdlib>
 #include <memory>
 #include <vector>
 #include <cstdint>
+#include <set>
 
 static void
 processRingItem(CRingItem& item);   // Forward definition, see below.
@@ -59,6 +62,8 @@ processRingItem(CRingItem& item);   // Forward definition, see below.
 // This is the list of items that are 'not good'.
 
 static std::vector<CPhysicsEventItem> badItems;
+static std::vector<std::string>       wrongness;
+
 static std::ofstream badfile("badItems.txt");
 
 /**
@@ -127,18 +132,204 @@ main(int argc, char** argv)
     
     std::exit(EXIT_SUCCESS);
 }
+/**
+ * goodItem
+ *    The item must:
+ *    - Have a timestamp that is 2 greater than the last one.
+ *    - Have two fragments with timestamp 1 greater than the last; one from
+ *      event source 0, and one from event source 2 then two fragments
+ *      with timestamp equal to the event timestamp, again  one from
+ *      sid 0 and one from sid 2.
+ *
+ * @param item - references the ring itme.
+ * @return bool - true if all criteria above are satisfied else false.
+ */
 
+static uint64_t lastTimestamp = 0;
 bool goodItem(CPhysicsEventItem& item)
 {
+    uint64_t expectedTimestamp = lastTimestamp + 2;
+    uint64_t eventTimestamp = item.getEventTimestamp();
+    if (expectedTimestamp != eventTimestamp) {
+        badItems.push_back(item);
+        std::stringstream reason;
+        reason << "Timestamp mismatch expected: " << expectedTimestamp
+            << " got " << eventTimestamp;
+        wrongness.push_back(reason.str());
+        lastTimestamp = eventTimestamp;      // Try to resynch.
+        return false;                        // No good timestamp.
+    }
+    lastTimestamp = eventTimestamp;          // do it here in case the goodness fails.
+    
+    // Get ready to examine the fragments:
+    
+    FragmentIndex frags(static_cast<uint16_t*>(item.getBodyPointer()));
+    
+    if (frags.getNumberFragments() != 4) {
+        badItems.push_back(item);
+        wrongness.push_back(std::string("There are not 4 fragments in this event"));
+        return false;
+    }
+    // First two frags should have expectedTimestamp -1 and be from sources 0,2
+    // though we don't know what order.
+    
+    std::set<uint32_t>   sids;
+    
+    FragmentInfo frag0 = frags.getFragment(0);
+    FragmentInfo frag1 = frags.getFragment(1);
+    uint64_t expectedFragTimestamp = expectedTimestamp -1;
+    if (
+        (frag0.s_timestamp != expectedFragTimestamp) ||
+        (frag1.s_timestamp != expectedFragTimestamp)
+    ) {
+        badItems.push_back(item);
+        std::stringstream reason;
+        reason << " Fragment 1/0 has an unexpected timestamp:  Should be: "
+            << expectedFragTimestamp << " frag 0 " << frag0.s_timestamp
+            << " frag1 " << frag1.s_timestamp;
+        wrongness.push_back(reason.str());
+        return false;
+    }
+    sids.emplace(frag0.s_sourceId);
+    sids.emplace(frag1.s_sourceId);
+    if ((sids.count(0) == 0) || (sids.count(2) == 0)) {
+        badItems.push_back(item);
+        std::stringstream reason;
+        reason << " First 2 frags should have sids 0, and 2 but got: "
+            << " frag0: " << frag0.s_sourceId << " frag1: " << frag1.s_sourceId;
+        wrongness.push_back(reason.str());
+        return false;
+    }
+    
+    // The last two frags (frag 2, 3) should both have the event timestamp
+    // as we're using last for the policy.  they too should have sids 0, 2.
+    
+    sids.clear();
+    FragmentInfo frag2 = frags.getFragment(2);
+    FragmentInfo frag3 = frags.getFragment(3);
+    if (
+        (frag2.s_timestamp != eventTimestamp)  || (frag3.s_timestamp != eventTimestamp)
+    ) {
+        badItems.push_back(item);
+        std::stringstream reason;
+        reason << " Fragment 2/3 has an  unexpected timestamp.  Should be: "
+            << eventTimestamp << " frag 2: " << frag2.s_timestamp
+            << " frag 3 " << frag3.s_timestamp;
+        wrongness.push_back(reason.str());
+        return false;
+    }
+    sids.emplace(frag2.s_sourceId);
+    sids.emplace(frag3.s_sourceId);
+    if ((sids.count(0) == 0) || (sids.count(2) == 0)) {
+        badItems.push_back(item);
+        std::stringstream reason;
+        reason << " Last 2 frags should have sids 0, and 2 but got: "
+            << " frag2: " << frag2.s_sourceId << " frag3: " << frag3.s_sourceId;
+        wrongness.push_back(reason.str());
+        return false;
+    }
+    
+    
+    
     return true;
 }
+/**
+ * dumpBadItem
+ *    Dumps a single bad item to the file.
+ *
+ * @param i - the bad item index.
+ */
+void dumpBadItem(int i)
+{
+    badfile << "------------------------------------------------\n";
+    badfile << wrongness[i] << std::endl;
+    badfile << badItems[i].toString() << std::endl;
+
+}
+/**
+ * dumpBadItems:
+ *    Dump the bad items  and their reasons to the output file badfile.
+ */
 void dumpBadItems()
 {
-    
+    if (badItems.size() != wrongness.size()) {
+        std::cerr << "Can't dumpBadItems because there's a mismatch between \n";
+        std::cerr << "the size of wrongness: " << wrongness.size()
+            << " and the size of badItems: " << badItems.size() << std::endl;
+        std::cerr << "This indicates an error in the goodItem() logic\n";
+        std::exit(EXIT_FAILURE);
+    }
+    for (int i = 0; i < wrongness.size(); i++) {
+        dumpBadItem(i);
+    }
 }
+/**
+ * At the end of the run, we _should_ have a sequence of bad items because
+ * one source runs longer than the others.  These items should:
+ * - differ in timestamp by 2 as before.
+ * - Have two fragments.
+ * - The fragments in all should be the same (from the longer  running source).
+ *
+ *  Any itesm which are not like this get dumped.
+ */
 void checkBadItemsAtEndRun()
 {
+    if (badItems.size() == 0) return;                  // Unlikely.
+    badfile << "---------------- Items before end of run ----------------\n";
     
+    // Establish the sourceid and base timestamp from the first bad item:
+    
+    CPhysicsEventItem first(badItems[0]);
+    uint64_t expectedStamp = first.getEventTimestamp() - 2;  // set up for loop.
+    FragmentIndex frags(static_cast<uint16_t*>(first.getBodyPointer()));
+    if(frags.getNumberFragments() == 0) {
+        badfile << "Checking end of run - first bad event has no fragments!!!!\n";
+        dumpBadItem(0);
+        std::exit(EXIT_FAILURE);
+    }
+    uint32_t expectedSid = frags.getFragment(0).s_sourceId;
+    if (wrongness.size() != badItems.size()) {
+        std::cerr << " End of run check: ";
+        std::cerr << "Can't dumpBadItems because there's a mismatch between \n";
+        std::cerr << "the size of wrongness: " << wrongness.size()
+            << " and the size of badItems: " << badItems.size() << std::endl;
+        std::cerr << "This indicates an error in the goodItem() logic\n";
+        std::exit(EXIT_FAILURE);
+    }
+    
+    // Now loop over all items.
+    
+    for (int i =0; i < badItems.size(); i++) {
+        expectedStamp += 2;
+        CPhysicsEventItem item(badItems[i]);
+        if (expectedStamp != item.getEventTimestamp()) {
+            badfile << "-------------------------- ERcheck\n";
+            badfile << " Event timestamp mismatch - expected: "
+                << expectedStamp  << " got " << item.getEventTimestamp()
+                << std::endl << item.toString() << std::endl;
+            expectedStamp = item.getEventTimestamp();
+            continue;
+        }
+        FragmentIndex frags(static_cast<uint16_t*>(item.getBodyPointer()));
+        if(frags.getNumberFragments() != 2) {
+            badfile << "--------------------------- ERcheck\n";
+            badfile << " Expected 2 fragments, but got " << frags.getNumberFragments()
+                << std::endl << item.toString() << std::endl;
+            continue;
+        }
+        FragmentInfo frag0 = frags.getFragment(0);
+        FragmentInfo frag1 = frags.getFragment(1);
+        uint32_t sid0 = frag0.s_sourceId;
+        uint32_t sid1 = frag1.s_sourceId;
+        
+        if ((sid0 != expectedSid) || (sid1 != expectedSid)) {
+            badfile << "------------------------- ERCheck\n";
+            badfile << " Not all fragment source ids were as expected: " << expectedSid
+              << " frag0: " << sid0 << " frag1: " << sid1 << std::endl;
+            badfile << item.toString() << std::endl;
+            continue;                    // In case we add tests later and forget.
+        }
+    }
 }
 
 /**
@@ -156,14 +347,13 @@ processRingItem(CRingItem& item)
     if (item.type() == PHYSICS_EVENT) {
         CPhysicsEventItem* pItem =
             dynamic_cast<CPhysicsEventItem*>(CRingItemFactory::createRingItem(item));
-        if (!goodItem(*pItem)) {
-            badItems.push_back(*pItem);
-        } else {
+        if (goodItem(*pItem)) {
             if (badItems.size()) {
                 dumpBadItems();
                 badItems.clear();
             }
         }
+        delete pItem;
     }
     // At the end of the run, we must ensure that all 'bad' items only have
     // contributions from one event source and that this source is the same for
@@ -174,3 +364,5 @@ processRingItem(CRingItem& item)
     }
     
 }
+
+void* gpTCLApplication(nullptr);
