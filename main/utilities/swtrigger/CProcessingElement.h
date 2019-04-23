1a8ecd24d6c7aa61fd71b8bcefe9198479c7e621
Ron Fox <fox@nscl.msu.edu>
2019-04-23 13:32:54 -0400

    ABC for doing some step of processing in a communicating parallel processing system.

---
+++ main/utilities/swtrigger/CProcessingElement.h
@@ -0,0 +1,43 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  CProcessingElement.h
+ *  @brief: Abstract base class for processing elements.
+ */
+
+#ifndef CPROCESSINGELEMENT_H
+#define CPROCESSINGELEMENT_H
+
+/**
+ * @class CProcessingElement
+ *   This is the abstract base class of a generic processing element.
+ *   The element has a flow of control descdribed by its
+ *   operator() and something that does processing described by
+ *   process().   The operator() obtains data for process() to
+ *   process, if processing results in any data process must arrange to
+ *   send it somewhere.
+ */
+class CProcessingElement
+{
+public:
+    virtual ~CProcessingElement();
+    
+    virtual void operator()()  = 0;
+    virtual void process(void* pData, size_t nBytes) = 0;
+};
+
+#endif
