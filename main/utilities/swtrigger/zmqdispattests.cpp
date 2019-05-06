1bb126080880c66d96c4caf0fa8829d7c845bcce
Ron Fox <fox@nscl.msu.edu>
2019-05-06 13:05:33 -0400

    Tested CDispatcher.

--- main/utilities/swtrigger/zmqdispattests.cpp
+++ main/utilities/swtrigger/zmqdispattests.cpp
@@ -15,18 +15,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <thread>
+#include <stdint.h>
+#include <sys/uio.h>
 #include <zmq.hpp>
 
 static const std::string router("inproc://router");
 static const std::string pusher("inproc://pusher");
 
+//  The dispatcher needs to run in a thread, here's a functor for that thread:
+
+class DispatThread {
+private:
+  CDispatcher*  m_pDispat;
+public:
+  DispatThread(CDispatcher* pDispat) : m_pDispat(pDispat) {}
+  void operator() () {
+    size_t size;
+    void*  pData;
+    do {
+      m_pDispat->receiveWorkItem(&pData, size);
+      m_pDispat->sendWorkItem(pData, size);
+      free(pData);
+    } while (size > 0);
+  }
+  
+};
+
 // A great test of the dispatcher, and a fairly normal use case is for it
 // to get data fanned out from a Router and to push it as a fan-in to a
 // puller.  We set that up here but as a pipeline only.
-
-class Testname : public CppUnit::TestFixture {
-  CPPUNIT_TEST_SUITE(Testname);
-  CPPUNIT_TEST(aTest);
+class dispatTest : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(dispatTest);
+  CPPUNIT_TEST(dispat_1);
   CPPUNIT_TEST_SUITE_END();
 
 
@@ -62,10 +83,42 @@ public:
     
   }
 protected:
-  void aTest();
+  void dispat_1();
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(Testname);
+CPPUNIT_TEST_SUITE_REGISTRATION(dispatTest);
 
-void Testname::aTest() {
+void dispatTest::dispat_1() {
+  DispatThread d(m_pTestObj);
+  std::thread t(std::ref(d));
+  
+  // Have the router send data to the dispatcher... pulller should get it.
+  
+  uint8_t msg[128];
+  for (int i =0; i < sizeof(msg); i++) {
+    msg[i] = i;
+  }
+  iovec m;
+  m.iov_base =  msg;
+  m.iov_len  = sizeof(msg);
+  
+  m_pRouter->send(&m, 1);
+  
+  void *pData;
+  size_t nBytes;
+  m_pPuller->recv(&pData, nBytes);
+  EQ(sizeof(msg), nBytes);
+  EQ(0, memcmp(msg, pData, sizeof(msg)));
+  free(pData);
+  
+  // Send the end to get the thread to exit -- puller also should get it:
+  
+  m_pRouter->end();
+  m_pPuller->recv(&pData, nBytes);
+  EQ(size_t(0), nBytes);
+  
+  
+  t.join();                        // Join the exited thread.
+  
 }
+
