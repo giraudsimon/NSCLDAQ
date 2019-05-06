eab828f6e2eb330159e792f2daf678c4594f044d
Ron Fox <fox@nscl.msu.edu>
2019-05-06 10:45:04 -0400

    Tested CReceiver

--- main/utilities/swtrigger/zmqreceivertests.cpp
+++ main/utilities/swtrigger/zmqreceivertests.cpp
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdint.h>
+#include <sys/uio.h>
 #include <zmq.hpp>
 
 static const std::string service("inproc://receivertest");
@@ -22,6 +23,7 @@ static const std::string service("inproc://receivertest");
 class receiverTest : public CppUnit::TestFixture {  // ZMQ Push/Pull.
   CPPUNIT_TEST_SUITE(receiverTest);
   CPPUNIT_TEST(recv_1);
+  CPPUNIT_TEST(recv_2);
   CPPUNIT_TEST_SUITE_END();
 
 
@@ -47,6 +49,7 @@ public:
   }
 protected:
   void recv_1();
+  void recv_2();
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(receiverTest);
@@ -66,4 +69,35 @@ void receiverTest::recv_1() {              // Single part message:
   EQ(0, memcmp(msg, pData, sizeof(msg)));
   
   free(pData);
+}
+void receiverTest::recv_2()
+{
+  uint8_t part1[100];
+  uint8_t part2[200];
+  for (int i = 0; i < sizeof(part1); i++) {
+    part1[i] = i;
+  }
+  for (int i =0; i < sizeof(part2); i++) {
+    part2[i] = sizeof(part2) - i;
+  }
+  iovec v[2];                    // parts descriptors.
+  v[0].iov_base = part1;
+  v[0].iov_len  = sizeof(part1);
+  v[1].iov_base = part2;
+  v[1].iov_len  = sizeof(part2);
+  
+  m_pSender->sendMessage(v, 2);
+  
+  void* pData;
+  size_t nBytes;
+  m_pTestObj->getMessage(&pData, nBytes);
+  
+  EQ(sizeof(part1) + sizeof(part2), nBytes);
+  EQ(0, memcmp(part1, pData,sizeof(part1)));
+  uint8_t* p2 = static_cast<uint8_t*>(pData);
+  p2 += sizeof(part1);
+  EQ(0, memcmp(part2, p2, sizeof(part2)));
+  
+  free(pData);
+  
 }
