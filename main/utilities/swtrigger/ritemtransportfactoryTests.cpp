1989fbbb2b7ed3fed4474a38ba978a340fc5aed0
Ron Fox <fox@nscl.msu.edu>
2019-05-07 13:34:29 -0400

    Setup/teardown and URI manufacturing methods produced.

--- main/utilities/swtrigger/ritemtransportfactoryTests.cpp
+++ main/utilities/swtrigger/ritemtransportfactoryTests.cpp
@@ -2,8 +2,39 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include "CRingItemTransportFactory.h"
+#include "CRingItemFileTransport.h"
+#include "CRingItemTransport.h"
+#include "CRingBuffer.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
+#include <string.h>
 
 
+static const std::string filenameTemplate("facttestXXXXXX");
+static const std::string ringName("facttest");
+
+static std::string makeFileURI(const char* pName)
+{
+  std::string result = "file://./";
+  result += pName;
+  
+  return result;
+}
+static std::string makeRingURI(const char* ringName)
+{
+  std::string result = "tcp://localhost/";
+  result += ringName;
+  
+  return result;
+}
+
+
+
+///
 
 class Testname : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(Testname);
@@ -12,11 +43,23 @@ class Testname : public CppUnit::TestFixture {
 
 
 private:
-
+  std::string m_filename;
+  int         m_fd;
+  
 public:
   void setUp() {
+    char tempName[filenameTemplate.size() + 1];
+    strcpy(tempName, filenameTemplate.c_str());
+    m_fd = mkstemp(tempName);
+    m_filename = tempName;             // mkstemp modified.
+    
+    CRingBuffer::create(ringName);
   }
   void tearDown() {
+    close(m_fd);
+    unlink(m_filename.c_str());
+    
+    CRingBuffer::remove(ringName);
   }
 protected:
   void aTest();
