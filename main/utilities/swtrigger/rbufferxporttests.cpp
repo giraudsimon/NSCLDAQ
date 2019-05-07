63161395939ee2fa5715223efa7bdfbb38f58746
Ron Fox <fox@nscl.msu.edu>
2019-05-07 08:22:38 -0400

    Got the setup/teardown done

--- main/utilities/swtrigger/rbufferxporttests.cpp
+++ main/utilities/swtrigger/rbufferxporttests.cpp
@@ -2,8 +2,17 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include "CRingBufferTransport.h"
+#include <CRingBuffer.h>
+#include <CRingBufferChunkAccess.h>
 
+#include <sys/uio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
 
+const std::string ringName("rbufferXportTest");
 
 class Testname : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(Testname);
@@ -12,11 +21,28 @@ class Testname : public CppUnit::TestFixture {
 
 
 private:
-
+  CRingBuffer*    m_produceTo;
+  CRingBuffer*    m_consumeFrom;
+  CRingBufferChunkAccess*  m_reader;
+  
+  CRingBufferTransport*  m_producer;
+  CRingBufferTransport*  m_consumer;
 public:
   void setUp() {
+    m_produceTo   = CRingBuffer::createAndProduce(ringName);
+    m_consumeFrom = new CRingBuffer(ringName);    // Default is consumer.
+    m_reader      = new CRingBufferChunkAccess(m_consumeFrom);
+    
+    m_producer = new CRingBufferTransport(*m_produceTo);
+    m_consumer = new CRingBufferTransport(*m_reader);
+    
   }
   void tearDown() {
+    delete m_producer;               // Deletes the production ring buffer obj.
+    delete m_consumer;               // deletes the chunk accessor but we need
+    delete m_consumeFrom;            // to delete the CRingBuffer.
+    
+    CRingBuffer::remove(ringName);   // Clean up the ring buffer itself.
   }
 protected:
   void aTest();
