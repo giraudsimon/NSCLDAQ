36571c29522464b408dc79cff677d7e0c06d9dda
Ron Fox <fox@nscl.msu.edu>
2019-05-08 10:10:21 -0400

    Setup/teardown complete.

--- main/utilities/swtrigger/sinktests.cpp
+++ main/utilities/swtrigger/sinktests.cpp
@@ -2,7 +2,13 @@
 
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include "CTestTransport.h"
+#include "CSender.h"
+#include "CReceiver.h"
+#include "CDataSinkElement.h"
 
+// We'll do this with test to test data stuff.
 
 
 class Testname : public CppUnit::TestFixture {
@@ -12,11 +18,27 @@ class Testname : public CppUnit::TestFixture {
 
 
 private:
-
+  CTestTransport*  m_pInputData;
+  CReceiver*       m_pReceiver;
+  CTestTransport*  m_pOutputData;
+  CSender*         m_pSender;
+  
+  CDataSinkElement* m_pTestObj;
+  
 public:
   void setUp() {
+    m_pInputData  = new CTestTransport;
+    m_pReceiver   = new CReceiver(*m_pInputData);
+    m_pOutputData = new CTestTransport;
+    m_pSender     = new CSender(*m_pOutputData);
+    
+    m_pTestObj    = new CDataSinkElement(*m_pReceiver, *m_pSender);
   }
   void tearDown() {
+    delete m_pTestObj;          // sender/receiver deleted but no transports:
+    
+    delete m_pInputData;
+    delete m_pOutputData;
   }
 protected:
   void aTest();
