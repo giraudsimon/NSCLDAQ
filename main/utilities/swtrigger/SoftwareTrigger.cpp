a5cd4533570062c0018fedbfd3e0dcda537bc05b
Ron Fox <fox@nscl.msu.edu>
2019-05-22 09:51:55 -0400

    Good compilation on initial SoftwareTrigger main program.

---
+++ main/utilities/swtrigger/SoftwareTrigger.cpp
@@ -0,0 +1,38 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2009.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/**
+* @file   SoftwareTrigger.cpp
+* @brief  Main program for the software trigger framwork.
+*/
+
+#include "swtriggerflags.h"
+#include <stdlib.h>
+
+/**
+ *  The main just processes the program files, instantiates the
+ *  application class and runs it.
+ *
+ * @param argc - number of command line parameters (includes invocation string).
+ * @param argv - Command line parameters.
+ */
+int main(int argc, char** argv)
+{
+    gengetopt_args_info params;
+    cmdline_parser(argc, argv, &params);
+    
+    exit(EXIT_SUCCESS);
+}
