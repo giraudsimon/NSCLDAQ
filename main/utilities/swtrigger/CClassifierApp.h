433c9fd17e5a47aa67465a81cb9677fcd8e91f9c
Ron Fox <fox@nscl.msu.edu>
2019-05-22 10:08:20 -0400

    Classifier application base class

---
+++ main/utilities/swtrigger/CClassifierApp.h
@@ -0,0 +1,48 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2009.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/**
+* @file  CClassifierApp.h
+* @brief ABC for classifier applications.
+*/
+#ifndef CCLASSIFIERAPP_H
+#define CCLASSIFIERAPP_H
+
+#include "swtriggerflags.h"
+
+/**
+ * @class CClassifierApp
+ *     The family of classifier applications embody both communication and
+ *     parallelization for the application. The idea is that the --parallel-stratgey
+ *     flag selects a specific communication and parallelization mechanism which,
+ *     in turn, can be used to instantiate the correct CClassifierApp subclass
+ *     by the SoftwareTrigger main program.
+ * @note In addition to providing an abstract base class for the hierarchy, the
+ *       class encapsulates the parsed program options.
+ */
+class CClassifierApp
+{
+protected:
+    gengetopt_args_info  m_params;
+public:
+    CClassifierApp(getgetopt_args_info& args) :
+        m_params(args) {}
+    virtual ~CClassifierApp() {}                     // for destructor chaining.
+    
+    virtual int operator()() = 0;                    // Run the application
+}
+
+#endif
