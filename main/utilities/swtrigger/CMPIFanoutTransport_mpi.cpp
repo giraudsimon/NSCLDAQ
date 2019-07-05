17659c5c7c0b622b676487a4346534a4073056a3
Ron Fox <fox@nscl.msu.edu>
2019-07-05 10:51:49 -0400

    Add an MPI based fanout transport

---
+++ main/utilities/swtrigger/CMPIFanoutTransport_mpi.cpp
@@ -0,0 +1,135 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  CMPIFanoutTransport_mpi.cpp
+ *  @brief: Implement the CMPIFanoutTransportClass.
+ */
+
+#include "CMPIFanoutTransport_mpi.h"
+
+#include <stdexcept>
+#include <stdlib.h>
+
+/**
+ * constructor (default)
+ *    Just construct the base class
+ */
+CMPIFanoutTransport::CMPIFanoutTransport() : CMPITransport() {}
+
+/**
+ * constructor with communicator.
+ *   Construct the base class and set its communicator.
+ *
+ * @param communicator - the MPI communicator to use.
+ */
+CMPIFanoutTransport::CMPIFanoutTransport(MPI_Comm communicator) :
+    CMPITransport()
+{
+    setCommunicator(communicator);        
+}
+
+/**
+ * destructor.
+ */
+CMPIFanoutTransport::~CMPIFanoutTransport()
+{
+    
+    end();                    // Don't exit with hanging clients.
+}
+/**
+ * recv
+ *    @throws std::logic_error - this transport is unidirectional.
+ */
+void
+CMPIFanoutTransport::recv(void** ppData, size_t& size)
+{
+    throw std::logic_error("CMPIFanoutTransport only can send!");
+}
+/**
+ * send
+ *    Get data request and send this data in response:
+ *    Note, getDataRequets registers the client and
+ *    sets the receiver in the base class.
+ *
+ *  @param parts - parts of the message
+ *  @param numParts - number of message parts.
+ */
+void
+CMPIFanoutTransport::send(iovec* parts, size_t numParts)
+{
+    getDataRequest();
+    CMPITransport::send(parts, numParts);
+}
+/**
+ * end
+ *   End of data handling.
+ *   While there are still clients, get the next data request
+ *   and respond to it with an end, removing that client
+ *   from the registry.  Note that the use of
+ *   getDataRequest ensures the registry always has the
+ *   client requesting data.
+ */
+void
+CMPIFanoutTransport::end()
+{
+    while (m_clients.empty()) {
+        getDataRequest();
+        end();
+        int client = setReceiver(-1);  // Gets set next getDataReq
+        m_clients.remove(client);
+    }
+}
+/////////////////////////////////////////////////////////////////
+// Private methods.
+//
+
+/**
+ * getDataRequest
+ *    Receives a data request from a client.
+ *    - Sets the receiver.
+ *    - If necessary, adds the receiver to the client registry.
+ *
+ *  @throw std::logic_error if the message received as not a
+ *                          data request.
+ */
+void
+CMPIFanoutTransport::getDataRequest()
+{
+    void* pData;
+    size_t nBytes;
+    
+    CMPITransport::recv(&pData, nBytes);
+    if (getLastReceivedTag() != dataRequestTag) {
+        if (nBytes) free(pData);   // In case we keep running
+        throw std::logic_error(
+            "CMPIFanoutTransport - received something other than a data request tag"
+        );
+    }
+    if (nBytes) {
+        free(pData);
+        throw std::logic_error(
+            "CMPITransport - nonempty data request"
+        );
+    }
+    int requestor = getLastReceivedRank();
+    setReceiver(requestor);
+    if (!m_clients.hasClient(requestor) ) {
+        m_clients.add(requestor);
+    }
+    
+    
+}
---
+++ main/utilities/swtrigger/CMPIFanoutTransport_mpi.h
@@ -0,0 +1,73 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  CMPIFanoutTransport_mpi.h
+ *  @brief: Define a fanout transport using MPI.
+ */
+#ifndef CMPIFANOUTTRANSPORT_MPI_H
+#define CMPIFANOUTTRANSPORT_MPI_H
+
+#include "CMPITransport_mpi.h"
+#include "CClientRegistry.h"
+#include <sys/uio.h>
+
+/**
+ * @class CMPIFanoutTransport
+ *    This class is the sender  transport for a fanout.
+ *    It pairs with CMPIFanoutClientTransport which receives data.
+ *    Each client requests a work item from this transport by
+ *    sending an empty message with the dataRequestTag tag.
+ *    - Receive operations throw an exception.
+ *    - Send operations receive a data request and then respond
+ *      to the requesting rank with the work item provided.
+ *    - End operations again wait for data requests and send end
+ *      messages until there are no more requestors.
+ *
+ *  @note CMPIFanoutClientTransport must use the same communicator
+ *        as the paired CMPIFanoutTransport.
+ *  @note as requests come for data they are registered in this
+ *        class's client registry.
+ *  @note the end code is crafted so that if a data request
+ *        comes from a client that has not yet registered,
+ *        it's still sent an end.  This allows for the case
+ *        when the data set is smaller than the number of
+ *        clients e.g.
+ *   @note ranks within the communicator are used to identify clients not some
+ *        client id set by the client transport.
+ *        
+ */
+class CMPIFanoutTransport : public CMPITransport
+{
+private:
+    CClientRegistry m_clients;
+public:
+    CMPIFanoutTransport();
+    CMPIFanoutTransport(MPI_Comm communicator);
+    virtual ~CMPIFanoutTransport();
+    
+    virtual void recv(void** ppData, size_t& size);
+    virtual void send(iovec* parts,  size_t numParts);
+    virtual void end();
+    
+    static const int dataRequestTag  = 2;   // Tag to use when requesting data from us.
+protected:
+    void getDataRequest();
+};
+
+
+
+#endif
