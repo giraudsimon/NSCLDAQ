66598c412032f5b6142c800124cecc7b4a692e96
Ron Fox <fox@nscl.msu.edu>
2019-08-01 08:55:32 -0400

    Added base class for application classes.

---
+++ main/utilities/swtrigger/CTransformerApp.cpp
@@ -0,0 +1,63 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  CTransformerApp.cpp
+ *  @brief: Implements the non-trivial utility methods of the  application
+ *          (common code between all application classes).
+ */
+
+#include "CTransformerApp.h"
+#include <dlfcn.h>
+#include <string>
+#include <stdexcept>
+
+/**
+ * getExtenderFactory
+ *     - Loads the user .so
+ *     - Locates the factory method in the .so and returns it.
+ *
+ * @return pointer to the factory funcstion
+ * @throw  std::runtime_error for various error conditions.
+ */
+ExtenderFactory
+CTransformerApp::getExtenderFactory()
+{
+    void* soHandle = dlopen(m_params.extendlib_arg, RTLD_NOW |RTLD_GLOBAL);
+    if (!soHandle) {
+        std::string error = dlerror();
+        std::string msg   = "Failed to open shared library: ";
+        msg += m_params.extendlib_arg;
+        msg += " ";
+        msg += error;
+        throw std::runtime_error(msg);
+    }
+    dlerror();                         // Clear errors (from manpage example).
+    void* rawFactory = dlsym(soHandle, "createExtender");
+    char* error = dlerror();
+    if (error != nullptr) {
+        std::string msg = "Unable to locate 'createExtender' in  ";
+        msg += m_params.extendlib_arg;
+        msg += " ";
+        msg += error;
+        msg += " be sure it's delcared extern \"C\"";
+        throw std::runtime_error(msg);
+    }
+
+    ExtenderFactory result = reinterpret_cast<ExtenderFactory>(rawFactory);
+    return result;
+   
+}
---
+++ main/utilities/swtrigger/CTransformerApp.h
@@ -0,0 +1,60 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  CTransformerApp.h
+ *  @brief: Base class for the application class for the Transformer program.
+ */
+#ifndef CTRANSFORMERAPP_H
+#define CTRANSFORMERAPP_H
+
+#include "transformer.h"
+#include "CBuiltRingItemExtender.h"
+
+typedef CBuiltRingItemExtender::CRingItemExtender Extender, *pExtender;
+typedef Extender* (*ExtenderFactory)();
+/**
+ * @class CTransformerApp
+ *    This is the base class of the application class for
+ *    the transformer application.  The application implementions are supposed to
+ *    - Set up processor objects and communications between them for a specific
+ *      transport family.
+ *    - Execute threads or the appropriate process (again depending on the
+ *      communications framework).
+ *
+ * This application typically constis of a
+ *     -   Data source element to get data from the source and fan it out to workers
+ *     -   Workers that call user code to transform the data.
+ *     -   A sort task into which the workers fan their data for timestamp re-ordering.
+ *     -   An output task that puts sorted data to the sink.
+ *
+ * \verbatim
+ *         source  - Worker farm - sorter - output (sink).
+ * \endverbatim
+ */
+class CTransformerApp
+{
+protected:
+    gengetopt_args_info m_params;
+public:
+    CTransformerApp(gengetopt_args_info p) : m_params(p) {}
+    virtual ~CTransformerApp() {}
+    
+    virtual void operator()() = 0;                // Concrete class implement this.
+protected:
+    ExtenderFactory getExtenderFactory();
+};
+#endif
