4d8a59f23cb0e8a1ac75c581ab36c499e9db2005
Ron Fox <fox@nscl.msu.edu>
2019-08-19 15:18:47 -0400

    Add minimal EventEditor bin target.

---
+++ main/utilities/swtrigger/EventEditor.cpp
@@ -0,0 +1,41 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  EventEditor.cpp
+ *  @brief: Main program for the event editor.
+ */
+
+
+#include "eventeditor.h"
+
+
+#include <stdlib.h>
+/**
+ * main
+ *    Entry point.
+ *    - Parse the parameters.
+ *    - Choose the application based on the --parallel-strategy flag.
+ *    - Instantiate and run the appropriate appliction.
+ */
+int
+main(int argc, char** argv)
+{
+    gengetopt_args_info parsed;
+    cmdline_parser(argc, argv, &parsed);
+    
+    exit(EXIT_SUCCESS);
+}
