7844306a70e5cbd1c2f62d39e14b27db77d32bb0
Ron Fox <fox@nscl.msu.edu>
2020-02-13 15:36:11 -0500

    Complete arena tests

--- main/ddas/sorter/arenaTests.cpp
+++ main/ddas/sorter/arenaTests.cpp
@@ -11,12 +11,18 @@
 #include "BufferArena.h"
 #undef private
 
+#include <stdexcept>
 
 class arenaTest : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(arenaTest);
   CPPUNIT_TEST(initial_1);
   
   CPPUNIT_TEST(alloc_1);
+  CPPUNIT_TEST(alloc_2);
+  CPPUNIT_TEST(alloc_3);
+  CPPUNIT_TEST(alloc_4);
+  CPPUNIT_TEST(alloc_5);
+  CPPUNIT_TEST(alloc_6);
   CPPUNIT_TEST_SUITE_END();
 
 
@@ -32,6 +38,12 @@ public:
 protected:
   void initial_1();
   void alloc_1();
+  void alloc_2();
+  void alloc_3();
+  void alloc_4();
+  void alloc_5();
+  void alloc_6();
+  
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(arenaTest);
@@ -57,6 +69,82 @@ void arenaTest::alloc_1()
   // should just be able to delete it:
   
   CPPUNIT_ASSERT_NO_THROW(
-    delete pBuffer;
+    delete pBuffer
   );
+}
+void arenaTest::alloc_2()
+{
+  // Allocation and freeing is ok -- if I never referenced.
+  // Freeing puts the buffer into the free buffer queue.
+  
+  DDASReadout::ReferenceCountedBuffer* pBuffer = m_pTestObj->allocate(100);
+  CPPUNIT_ASSERT_NO_THROW (
+    m_pTestObj->free(pBuffer)
+  );
+  
+  EQ(size_t(1), m_pTestObj->m_BufferPool.size());
+  EQ(pBuffer, m_pTestObj->m_BufferPool.front());
+  
+  
+}
+void arenaTest::alloc_3()
+{
+  // Freeing a referenced buffer is a logic error.
+  
+   DDASReadout::ReferenceCountedBuffer* pBuffer = m_pTestObj->allocate(100);
+   pBuffer->reference();
+   CPPUNIT_ASSERT_THROW(
+    m_pTestObj->free(pBuffer),
+    std::logic_error
+   );
+   
+   pBuffer->dereference();          // Now I can free it.
+   CPPUNIT_ASSERT_NO_THROW(
+    m_pTestObj->free(pBuffer)
+   );
+}
+void arenaTest::alloc_4()
+{
+  // Buffers get re-used if they are big enough:
+  
+  DDASReadout::ReferenceCountedBuffer* pBuffer = m_pTestObj->allocate(100);
+  m_pTestObj->free(pBuffer);
+  
+  DDASReadout::ReferenceCountedBuffer* pReused = m_pTestObj->allocate(100);
+  EQ(pBuffer, pReused);
+  m_pTestObj->free(pReused);
+}
+void arenaTest::alloc_5()
+{
+  // Re-use happens as long as the buffer is _at_least_ as big as the request.
+  
+ // Buffers get re-used if they are big enough:
+  
+  DDASReadout::ReferenceCountedBuffer* pBuffer = m_pTestObj->allocate(100);
+  m_pTestObj->free(pBuffer);
+  
+  DDASReadout::ReferenceCountedBuffer* pReused = m_pTestObj->allocate(50);
+  EQ(pBuffer, pReused);
+  m_pTestObj->free(pReused);
+}  
+void arenaTest::alloc_6()
+{
+  // Rallocation of the front is useed if neeed to
+  // Get the right size.  This test will force the
+  // reallocation.
+  
+  DDASReadout::ReferenceCountedBuffer* pBuf1 = m_pTestObj->allocate(100);
+  DDASReadout::ReferenceCountedBuffer* pBuf2 = m_pTestObj->allocate(500);
+  DDASReadout::ReferenceCountedBuffer* pBuf3 = m_pTestObj->allocate(100);
+  
+  // we'll make the 500  be bracked in the free list by the two 100s':
+  
+  m_pTestObj->free(pBuf1);
+  m_pTestObj->free(pBuf2);
+  m_pTestObj->free(pBuf3);
+  
+  
+  
+  DDASReadout::ReferenceCountedBuffer* pReused = m_pTestObj->allocate(300);
+  EQ(pBuf1, pReused);
 }
