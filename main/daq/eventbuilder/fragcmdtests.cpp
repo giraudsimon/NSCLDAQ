8f6bde87a879dedfad27d8e92043fa02768517bc
Ron Fox <fox@nscl.msu.edu>
2020-06-09 12:59:54 +0000

    Full template for test stuite for CFragmentHandlerCommand::operator()

---
+++ main/daq/eventbuilder/fragcmdtests.cpp
@@ -0,0 +1,53 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  fragcmdtests.cpp
+ *  @brief: Test the CFragmentHandlerCommand.cpp
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+#include "fragcmdstubs.h"
+#include "CFragmentHandlerCommand.h"
+#include <tcl.h>
+#include "TCLInterpreter.h"
+
+class fragcmdtest : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(fragcmdtest);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+
+public:
+    void setUp() {
+        
+    }
+    void tearDown() {
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(fragcmdtest);
+
+void fragcmdtest::test_1()
+{
+}
+
+void* gpTCLApplication(nullptr);
