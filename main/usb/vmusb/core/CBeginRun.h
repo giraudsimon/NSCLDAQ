433150d2a9c6bee8cf970e80345e8bbbbdf0cac5
Ron Fox <fox@nscl.msu.edu>
2020-06-11 11:04:29 +0000

    Making exportController static allows simple testing.

--- main/usb/vmusb/core/CBeginRun.h
+++ main/usb/vmusb/core/CBeginRun.h
@@ -58,7 +58,7 @@ protected:
   virtual int operator()(CTCLInterpreter& interp,
 			 std::vector<CTCLObject>& objv);
 private:
-  void exportController(CVMUSB* pController, CConfiguration* pConfig);
+  static void exportController(CVMUSB* pController, CConfiguration* pConfig);
 
 };
 
