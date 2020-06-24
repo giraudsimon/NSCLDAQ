50ced415a819d0ec01d822150a9e495371ca83de
Ron Fox <fox@nscl.msu.edu>
2020-06-24 17:00:53 +0000

    daqdev/NSCLDAQ#804 - Test CFilterTestSource so we know we can determine what the filter app class gets in our test for multi-inserts.

--- main/utilities/filter/testmultiple.cpp
+++ main/utilities/filter/testmultiple.cpp
@@ -21,27 +21,85 @@
 #include <cppunit/extensions/HelperMacros.h>
 #include <cppunit/Asserter.h>
 #include "Asserts.h"
+#include "CFilterTestSource.h"
+#include "CFilterTestSink.h"
+#include <CDataFormatItem.h>
+#include <CGlomParameters.h>
+#include <CRingStateChangeItem.h>
+#include <time.h>
 
 class testmultiple : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(testmultiple);
-    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST(src_1);
+    CPPUNIT_TEST(src_2);
+    CPPUNIT_TEST(src_3);
     CPPUNIT_TEST_SUITE_END();
-    
-private:
 
+protected:
+    void src_1();
+    void src_2();
+    void src_3();
+private:
+    CFilterTestSource* m_pSrc;
+    CFilterTestSink*   m_pSink;
 public:
     void setUp() {
-        
+        m_pSrc = new CFilterTestSource;
+        m_pSink= new CFilterTestSink;
     }
     void tearDown() {
-        
+        delete m_pSrc;
+        delete m_pSink;
     }
-protected:
-    void test_1();
+
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION(testmultiple);
 
-void testmultiple::test_1()
+void testmultiple::src_1()
 {
+    // Empty data source is an immediate eof:
+    
+    EQ((CRingItem*)(nullptr), m_pSrc->getItem());
+}
+void testmultiple::src_2()
+{
+    // If a put a ring item in the first returned item should
+    
+    // be equal to the one I took out.  We'll us a ring format item.
+    
+    CDataFormatItem item;
+    m_pSrc->addItem(&item);
+    
+    CRingItem* pItem = m_pSrc->getItem();
+    ASSERT(item == (*pItem));
+    
+    delete pItem;
+    
+    EQ((CRingItem*)(nullptr), m_pSrc->getItem());  // No more.
+}
+void testmultiple::src_3()
+{
+    // If a put a few items in the src they come out in the right order:
+    
+    CDataFormatItem item1;
+    CGlomParameters item2(100, true, CGlomParameters::first);
+    CRingStateChangeItem item3(BEGIN_RUN, 1, 0, time(nullptr), "The title string");
+    CRingStateChangeItem item4(END_RUN, 1, 100, time(nullptr), "The title string");
+    std::vector<CRingItem*> items = {&item1, &item2, &item3, &item4};
+    
+    m_pSrc->addItem(&item1);
+    m_pSrc->addItem(&item2);
+    m_pSrc->addItem(&item3);
+    m_pSrc->addItem(&item4);
+    
+    for (int i =0; i < items.size(); i++ ) {
+        CRingItem* p = m_pSrc->getItem();
+        ASSERT(p);
+        ASSERT(*p == *items[i]);
+        
+        delete p;
+    }
+    ASSERT(!m_pSrc->getItem());
+
 }
