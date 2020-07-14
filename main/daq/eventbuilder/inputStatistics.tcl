558476891990680f2e283cd6a97603422a587c30
Ron Fox <fox@nscl.msu.edu>
2020-07-14 14:04:53 +0000

    daqdev/NSCLDAQ#700 - another occurence of EVB::test::updateWidgetOption centralized.

--- main/daq/eventbuilder/inputStatistics.tcl
+++ main/daq/eventbuilder/inputStatistics.tcl
@@ -804,21 +804,3 @@ proc ::EVB::test::inputStatistics::queueDisplay {} {
     
 }
 
-
-##
-# Testing utility to update a widget option from a trace on a variable
-# that has a name matching the option:
-#
-# @param widget - the widget to modify.
-# @param name1  - name of the variable (and option)
-# @param name2  - index of the variable if an array.
-# @param op     - operation performed.
-#
-
-if {[info proc ::EVB::test::updateWidgetOption] eq ""} {
-    proc ::EVB::test::updateWidgetOption {widget name1 name2 op} {
-        upvar #0 $name1 value
-        $widget configure $name1 $value
-        return ""
-    }
-}
