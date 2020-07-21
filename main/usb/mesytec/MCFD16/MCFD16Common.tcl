037666d2ee3a5e32d76abe3161a1245b24d63ad5
Ron Fox <fox@nscl.msu.edu>
2020-07-21 12:30:43 +0000

    daqdev/NSCLDAQ#700 *  Fix test that will no longer work with runglobally now factored out. *  Factor out trigger bit computation from the USB and RC drivers.

--- main/usb/mesytec/MCFD16/MCFD16Common.tcl
+++ main/usb/mesytec/MCFD16/MCFD16Common.tcl
@@ -40,6 +40,7 @@ namespace eval MCFD16Common {
         }
     }
     ##
+
     # computeTriggerBits
     #    Given a trigger source/veto, returns the appropriate
     #    value to write as the trigger bits.
@@ -56,6 +57,11 @@ namespace eval MCFD16Common {
         if {$source ni [dict keys $sourceBits]} {
             set msg "Invalid source provided. Must be or, multiplicity, pair_coinc, mon, pat_or_0, pat_or_1, or gg."
             return -code error -errorinfo MCFD16XX::SetTriggerSource $msg
+
+        set sourceBits [dict create or 1 multiplicity 2 pair_coinc 4 mon 8 pat_or_0 16 pat_or_1 32 gg 128]
+        if {$source ni [dict keys $sourceBits]} {
+            set msg "Invalid source provided. Must be or, multiplicity, pair_coinc, mon, pat_or_0, pat_or_1, or gg."
+            return -code error -errorinfo MCFD16USB::SetTriggerSource $msg
         }
 
         set value [dict get $sourceBits $source]
@@ -106,4 +112,6 @@ namespace eval MCFD16Common {
             return -code error -errorinfo MCFD16XX::SetTriggerOrPattern $msg
         }
     }
+=======
+>>>>>>> daqdev/NSCLDAQ#700
 }
