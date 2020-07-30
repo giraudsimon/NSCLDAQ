b9569da7bfae3d413cba9821f9cc9a2dca7fd7b0
Ron Fox <fox@nscl.msu.edu>
2020-07-30 16:29:06 +0000

    daqdev/NSCLDAQ#700 - factored out command queue processing so there's no repeated code in pauseDAQ - before the entire command processing code was duplicted.

--- main/usb/vmusb/core/CAcquisitionThread.h
+++ main/usb/vmusb/core/CAcquisitionThread.h
@@ -80,7 +80,7 @@ protected:
   virtual void operator()();
 private:
   void mainLoop();
-  void processCommand(CControlQueues::opCode command);
+  void processCommand(CControlQueues::opCode command, bool running = true);
   void processBuffer(DataBuffer* pBuffer);
   void startDaq();
 public:
