3507ceb643d57821242f51703a6fd20cf20219b7
Ron Fox <fox@nscl.msu.edu>
2020-10-15 14:50:30 +0000

    daqdev/NSCLDAQ#975 NSCLDAQ SUPPORTED LOGBOOK
    Completed testing of LogBook.find_shift (instance method).

--- main/utilities/logbook/pyshifttests.py
+++ main/utilities/logbook/pyshifttests.py
@@ -75,7 +75,54 @@ class aTest(unittest.TestCase) :
         found = self.logbook.get_shift(shift1.id)
         self.assertEqual(shift1.id, found.id)
         
-        pass
+    def test_list1(self, ):
+        # No shifts:
+        
+        self.assertEqual(0, len(self.logbook.list_shifts()))
+    
+    def test_list2(self, ):
+        # Lists the one shift defined.
+        
+        shift2 = self.logbook.create_shift('non-doctorates', (self.ron, self.ava))
+        listing = self.logbook.list_shifts()
+        self.assertEqual(1, len(listing))
+        self.assertEqual(shift2.id, listing[0].id)
+        
+    def test_list3(self, ):
+        # Multiple shifts:
+        
+        shift1 = self.logbook.create_shift('professors', self.logbook.find_people('salutation = "Prof."'))
+        shift2 = self.logbook.create_shift('non-doctorates', (self.ron, self.ava))
+        
+        listing = self.logbook.list_shifts();
+        self.assertEqual(2, len(listing))
+        self.assertEqual(shift1.id, listing[0].id)
+        self.assertEqual(shift2.id, listing[1].id)
+        
+    def test_find1(self, ):
+        # Find shift by name: Nosuch gives None
+        
+        self.assertRaises(LogBook.error, self.logbook.find_shift, 'no such')
+       
+        
+    def test_find2(self, ):
+        shift1 = self.logbook.create_shift('professors', self.logbook.find_people('salutation = "Prof."'))
+        found = self.logbook.find_shift('professors')
+        self.assertEqual(shift1.id, found.id)
+        
+    def test_find3(self, ):
+        shift1 = self.logbook.create_shift('professors', self.logbook.find_people('salutation = "Prof."'))
+        shift2 = self.logbook.create_shift('non-doctorates', (self.ron, self.ava))
+        
+        found = self.logbook.find_shift('non-doctorates')
+        self.assertEqual(shift2.id, found.id)
+        self.assertRaises(LogBook.error, self.logbook.find_shift, "Professors")  # miscased.
+        
+    
+    
+    
+    
+    
     
         
         
