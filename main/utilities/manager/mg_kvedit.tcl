e7f69abe53fd056ee3c888f7de740ee3e28777c0
Ron Fox <fox@nscl.msu.edu>
2021-03-17 17:25:26 +0000

    daqdev/NSCLDAQ#1063 NSCLDAQ MANAGER SOFTWARE
    Build the kvlist widget.

--- main/utilities/manager/mg_kvedit.tcl
+++ main/utilities/manager/mg_kvedit.tcl
@@ -31,3 +31,116 @@ package require kvstore
 package require sqlite3
 package require Tk
 package require snit
+
+#-------------------------------------------------------------------------------
+# Megawidgets.
+#
+
+
+##
+# @class KvList
+#    Provides a list object that displays key value pairs.  The GUi is a
+#    ttk::treeview with key and value columns.  A vertical scrollbar facilitates
+#    vertical scrolling.  We also provide means to process selections of rows
+#    editing items, deleting items and adding items.
+#
+# OPTIONS
+#    -kvpairs   - Dict of key value pairs: Note the UI is loaded sorted by key.
+#    -selectcommand - Script invoked by a selection.
+# METHODS
+#   add    - adds a new key/value pair.
+#   delete - Removes a key
+#   modify - Modifies an existing key/value pair.
+#
+#
+snit::widgetadaptor KvList {
+    option -kvpairs -default [dict create]  \
+        -configuremethod _cfgContents -cgetmethod _cgetContents
+    option -selectcommand -default [list]
+    
+    component tree
+    delegate option * to tree
+    delegate method * to tree
+    
+    constructor args {
+        installhull using ttk::frame
+        
+        install tree using ttk::treeview $win.tree -columns [list key value] \
+            -displaycolumns [list key value] -show [list headings] \
+            -yscrollcommand [list $win.sb set] -selectmode browse
+        foreach col [list key value] title [list Key Value] {
+            $tree heading $col -text $title
+        }
+        bind $tree <<TreeviewSelect>> [mymethod _dispatchSelection]
+        
+        ttk::scrollbar $win.sb -orient vertical -command [list $tree yview]
+        
+        grid $tree $win.sb -sticky nsew
+        
+        $self configurelist $args
+        
+    }
+    
+    #--------------------------------------------------------------------------
+    # Configuation management
+    
+    ##
+    # _cfgContents
+    #     Configures the contents of the tree view.
+    #     Note that the keys are sorted alphabetically.  Thus a simple resort is
+    #     .list configure -kvpairs [.list cget -kvpairs]
+    #
+    # @param optname  - name of the option.
+    # @param value    - This is a dict whose keys are the keys and values the values.
+    #
+    method _cfgContents {optname value} {
+        $tree delete [$tree children {}]
+        
+        set keys [dict keys $value]
+        set keys [lsort -increasing -dictionary $keys]
+        
+        foreach key $keys {
+            set v [dict get $value $key]
+            $tree insert  {} end -values [list $key $v]
+        }
+    }
+    ##
+    # _cgetContents
+    #    Returns the key value pairs the widget has in its list.
+    # @param optname - option name.
+    # @return dict   - with key/value pairs that match the dict's.
+    method _cgetContents {optname} {
+        set items [$tree children {}]
+        set result [dict create]
+        foreach item $items {
+            set kvpair [$tree item $item -values]
+            dict set result [lindex $kvpair 0] [lindex $kvpair 1]
+        }
+        return $result
+    }
+    #--------------------------------------------------------------------------
+    # Event handling
+    
+    ##
+    # _dispatchSelection
+    #
+    #     Called on a <<TreeviewSelect>> virtual event.
+    #     Get the contents of the selected element, if there is one and
+    #     call the user's handler if there is one.
+    #
+    method _dispatchSelection {} {
+        set script $options(-selectcommand)
+        if {$script ne ""} {
+            set selection [$tree selection]
+            if {$selection ne ""} {
+                set v [$tree item $selection -values]
+                set param [dict create [lindex $v 0] [lindex $v 1]]
+            } else {
+                set param [dict create]
+            }
+            lappend script $param
+            uplevel #0 $script
+        }
+    }
+}
+    
