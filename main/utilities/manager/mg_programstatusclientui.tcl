796976d90bca8f6d90045bbca43b0f52d1efe3ad
Ron Fox <fox@nscl.msu.edu>
2021-04-07 14:54:45 +0000

    daqdev/NSCLDAQ#1063 NSCLDAQ MANAGER SOFTWARE
    Provided and tested capability to process -containers option
    to load the containers tree.

--- main/utilities/manager/mg_programstatusclientui.tcl
+++ main/utilities/manager/mg_programstatusclientui.tcl
@@ -62,8 +62,8 @@ snit::widgetadaptor ContainerStatusList {
     constructor {args} {
         installhull using ttk::frame
         
-        set colnames [list name image binding host]
-        set coltitles [list Name Image Binding Host]
+        set colnames [list  image binding host]
+        set coltitles [list Image Binding Host]
         
         install tree using ttk::treeview $win.tree                 \
             -columns $colnames                                     \
@@ -80,10 +80,70 @@ snit::widgetadaptor ContainerStatusList {
         grid columnconfigure $win 0 -weight 1;   # Allow the treeview to grow
         grid columnconfigure $win 1 -weight 0;   # But not the scrollbar.
         
-        
         $self configurelist $args
         
     }
+    #------------------------------------------------------------------------
+    # Private utility methods.
+    #
+    
+    ##
+    # _addContainer
+    #    Add a container to the end of the set of things in the tree.#
+    #      Top level has name, image, bindings where name is the tree.
+    #      second level is bindings and activations in the tree level
+    #      etc.
+    #
+    # @param item - dictionary item that describes the container to add.
+    #
+    method _addContainer {item} {
+        set name [dict get $item name]
+        set image [dict get $item image]
+        
+        set toplevel [$tree insert {} end -text $name -values $image -tags container]
+        
+        #  The bindings:
+        
+        set bindings [$tree insert $toplevel end -text bindings -tags bindcontainer]
+        foreach binding [dict get $item bindings] {
+            $tree insert $bindings end -values [list "" $binding] -tags binding
+        }
+        # Any activiations:
+        
+        set activations [$tree insert $toplevel end -text activations -tags activationcontainer]
+        foreach host [dict get $item activations] {
+            $tree insert $activations end -values [list "" "" $host] -tags activation
+        }
+        
+        
+    }
+    
+    #------------------------------------------------------------------------
+    #  Configuration handling
+    
+    ##
+    # _cfgContainers
+    #    Called when -containers is configured.
+    #    - Clear the tree
+    #    - Repopulate it using the new value.
+    # @param optname - option name.
+    # @param optval  - new option value.
+    # @note  options(optname) is set so we don't need a cget operation.
+    #
+    method _cfgContainers {optname optval} {
+        foreach child [$tree children {}] {
+            $tree delete $child;    # Deletes the subierarchy.
+        }
+        
+        foreach item $optval {
+            $self _addContainer $item
+        }
+        # If there are no errors, we can update:
+        
+        set options($optname) $optval
+    }
+    
+    #  Public methods
 }
     
 
