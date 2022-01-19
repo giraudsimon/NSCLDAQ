50b3319e89ca2eb82331564087e1ba5562d062ea
Ron Fox <fox@nscl.msu.edu>
2022-01-19 11:58:12 +0000

    Ensure that .bashrc is run on the remote systems.

--- main/daq/readoutgui/rsh.tcl
+++ main/daq/readoutgui/rsh.tcl
@@ -92,7 +92,7 @@ namespace eval  ssh {
 		
 		set container $::env(SING_IMAGE)
 		set bindings  [ssh::getSingularityBindings]
-		return "SING_IMAGE=$container singularity exec $bindings $container $command"
+		return "SING_IMAGE=$container singularity exec $bindings $container bash -c $command"
 	}
 	proc shellCommand { } {
         if {[array names ::env SING_IMAGE] eq ""} {
@@ -105,7 +105,7 @@ namespace eval  ssh {
 		
 		set container $::env(SING_IMAGE)
 		set bindings  [ssh::getSingularityBindings]
-		return "SING_IMAGE=$container singularity shell $bindings $container "
+		return "SING_IMAGE=$container singularity shell --shell /bin/bash $bindings $container "
     }
     #-------------------------------------------------------------------------
     proc ssh {host command} {
