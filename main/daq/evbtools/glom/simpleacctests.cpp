5aec8a53c1a993bd0be77bf0f9b7da4d998d6a95
Ron Fox <fox@nscl.msu.edu>
2022-02-21 08:27:55 -0500

    Initial test template for CEventAccumulatorSimple

---
+++ main/daq/evbtools/glom/simpleacctests.cpp
@@ -0,0 +1,79 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2017.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Authors:
+             Ron Fox
+             Giordano Cerriza
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+/** @file:  simpleacctests.cpp
+ *  @brief: 
+ */
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/Asserter.h>
+#include "Asserts.h"
+
+// Support white box testing.
+
+#define private public
+#include "CEventAccumulatorSimple.h"
+#undef private
+#include <string>
+#include <sys/mman.h>    // where memfd_create lives in buster.
+#include <unistd.h>
+
+// memory file name - we need something
+
+const std::string memoryFilename("output");
+
+// Default event accumulator settings.
+// Note there are tests that will delete the default one
+// and create a new one with different settings.
+// These are the settings that will be used in setUp
+
+const time_t      maxFlushTime(1);
+const size_t      bSize(1024);
+const size_t      maxfrags(10);
+const CEventAccumulatorSimple::TimestampPolicy policy(
+            CEventAccumulatorSimple::first
+);
+
+class aTestSuite : public CppUnit::TestFixture {
+    CPPUNIT_TEST_SUITE(aTestSuite);
+    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST_SUITE_END();
+    
+private:
+    int m_fd;
+    CEventAccumulatorSimple* m_pacc;
+public:
+    void setUp() {
+        m_fd = memfd_create(memoryFilename.c_str(), 0);
+        ASSERT(m_fd >= 0);
+        m_pacc = new CEventAccumulatorSimple(
+            m_fd, maxFlushTime, bSize, maxfrags, policy
+        );
+    }
+    void tearDown() {
+        delete m_pacc;
+        close(m_fd);
+        
+    }
+protected:
+    void test_1();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(aTestSuite);
+
+void aTestSuite::test_1()
+{
+}
