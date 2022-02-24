794b3d6d19840976f9c195de23cbc5979d92cbee
Ron Fox <fox@nscl.msu.edu>
2022-02-24 14:00:36 -0500

    Actual success in running Glom over a pipe.

--- main/daq/evbtools/glom/integrationtests.cpp
+++ main/daq/evbtools/glom/integrationtests.cpp
@@ -28,28 +28,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdint.h>
+#include <DataFormat.h>
 
 static  const char* argv[] = {
-    "./glom", "--dt=100", "--timestamp-policy=first", "--sourceid=10",
+    "./glom", "--dt=100", "--timestamp-policy=earliest", "--sourceid=10",
     "--maxfragments=100", nullptr
 };
 static const char* glom="./glom";
 
 class integrationtest : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(integrationtest);
-    CPPUNIT_TEST(test_1);
+    CPPUNIT_TEST(nothing_1);
     CPPUNIT_TEST_SUITE_END();
     
 private:
     pid_t m_glomPid;
-    int m_pipes[2];         // m_pipes[0] - data to glom glom m_pipes[1] output from glom.
+    int m_stdinpipe[2];         // glom reads 0, we write 1
+    int m_stdoutpipe[2];        //glom writes 1 we read 0.
 public:
     void setUp() {
         m_glomPid = startGlom();
     }
     void tearDown() {
-        close(m_pipes[0]);         // This EOF will klll off glom.
-        close(m_pipes[1]);
+        close(m_stdinpipe[1]);         // This EOF will klll off glom.
+        close(m_stdoutpipe[0]);
         if (m_glomPid > 0) {
             int exitStatus;
             EQ(waitpid(m_glomPid, &exitStatus, 0), m_glomPid);
@@ -57,7 +60,7 @@ public:
             
     }
 protected:
-    void test_1();
+    void nothing_1();
 private:
     pid_t startGlom();
 };
@@ -70,21 +73,30 @@ private:
 pid_t
 integrationtest::startGlom()
 {
-    ASSERT(pipe2(m_pipes, O_DIRECT) == 0);
+    ASSERT(pipe2(m_stdinpipe, O_DIRECT) == 0);
+    ASSERT(pipe2(m_stdoutpipe, 0) == 0);
     pid_t pid = fork();
     ASSERT(pid >= 0);
     if (pid > 0) {                  // parent
+        close(m_stdinpipe[0]);
+        close(m_stdoutpipe[1]);
         return pid;
     } else {                       // child
-        // m_pipes[0] becomes stdin and
-        // m_pipes[1] becomes stdout:
+        // m_stdinpipe[0] is our stdin.
+
         
-        int status = dup2(m_pipes[0], STDIN_FILENO);
+        close(STDIN_FILENO);
+        close(m_stdinpipe[1]);
+        int status = dup2(m_stdinpipe[0], STDIN_FILENO);
         if (status == -1) {
             perror("Glom dup of pipe[0] -> stdin failed");
             exit(EXIT_FAILURE);
         }
-        status = dup2(m_pipes[1], STDOUT_FILENO);
+        // stdoutpipe[1] becomes stdout:
+        
+        close(STDOUT_FILENO);
+        close(m_stdoutpipe[0]);
+        status = dup2(m_stdoutpipe[1], STDOUT_FILENO);
         if (status == -1) {
             perror("Glom dup of pipe[1] -> stdout failed");
             exit(EXIT_FAILURE);
@@ -101,6 +113,24 @@ integrationtest::startGlom()
 
 CPPUNIT_TEST_SUITE_REGISTRATION(integrationtest);
 
-void integrationtest::test_1()
+// closing Gloms input makes glom exit sending nothing.
+void integrationtest::nothing_1()
 {
+    close(m_stdinpipe[1]);   // EOF to glom's stin.
+    uint8_t buffer[1024];
+    ssize_t n = read(m_stdoutpipe[0], buffer, sizeof(buffer));
+    if (n < 0) {
+        perror("Failed read");
+    }
+    EQ(sizeof(DataFormat), size_t(n));
+    pDataFormat p = reinterpret_cast<pDataFormat>(buffer);
+    EQ(uint32_t(sizeof(DataFormat)),p->s_header.s_size);
+    EQ(RING_FORMAT, p->s_header.s_type);
+    EQ(FORMAT_MAJOR, p->s_majorVersion);
+    EQ(FORMAT_MINOR, p->s_minorVersion);
+    
+    int status;
+    EQ(m_glomPid, waitpid(m_glomPid, &status, 0));
+    
+    m_glomPid = 0;
 }
